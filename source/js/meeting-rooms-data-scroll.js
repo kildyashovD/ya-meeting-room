4c8c8b22c3368ff1be8de5eb951f8e597d9b17d6
Kildyashov <denis.kildiaschov@yandex.ru>
2018-07-15 21:01:32 +0300

    Анимирует горизонтальный скролл для meeting-rooms__data

---
+++ source/js/meeting-rooms-data-scroll.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var scrollElement = document.querySelector(".main-content");
+var roomTextData = document.querySelectorAll(".meeting-rooms__text-content");
+var roomSize = document.querySelectorAll(".meeting-rooms__size");
+
+scrollElement.addEventListener("scroll", function () {
+
+    for (var i = 0; i < roomSize.length; i++)
+    roomSize[i].classList.toggle("meeting-rooms__size--hide", scrollElement.scrollLeft > 121);
+
+    for (var j = 0; j < roomTextData.length; j++) {
+      roomTextData[j].classList.toggle("meeting-rooms__text-content--mob-scroll", scrollElement.scrollLeft > 121);
+    }
+
+}, false);
