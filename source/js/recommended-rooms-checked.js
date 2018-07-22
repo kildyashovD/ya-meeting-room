508a2e709001c918ea0bc2fc4573e6c6beb9e1c2
Kildyashov <denis.kildiaschov@yandex.ru>
2018-07-22 21:31:21 +0300

    Доделывает рекомендованные переговорки

--- source/js/recommended-rooms-checked.js
+++ source/js/recommended-rooms-checked.js
@@ -1,18 +1,23 @@
 var recommendedRoomsBlock = document.querySelector(".recommended-rooms");
 var recommendedRoomsLabel = document.querySelectorAll(".recommended-rooms__item");
+var recommendedCancel = document.querySelector(".recommended-rooms__item-delete");
 
 recommendedRoomsBlock.addEventListener("click", function(evt) {
   evt.preventDefault();
   var targetElement = evt.target;
 
-  while (targetElement !== this) {
-    if (targetElement.classList.contains("recommended-rooms__item")) {
-      checkRoom (targetElement);
-      this.setAttribute("data-heading", "Ваша переговорка");
-      return;
+  if (this.classList.contains("recommended-rooms--choose-js")) {
+    while (targetElement !== this) {
+      if (targetElement.classList.contains("recommended-rooms__item")) {
+        checkRoom (targetElement);
+        this.setAttribute("data-heading", "Ваша переговорка");
+      };
+      targetElement = targetElement.parentNode;
     };
-    targetElement = targetElement.parentNode;
-  };
+    this.classList.remove("recommended-rooms--choose-js");
+  } else if (targetElement.classList.contains("recommended-rooms__item-delete")) {
+    cancelRoom ();
+  }
 });
 
 function checkRoom (element) {
@@ -23,3 +28,17 @@ function checkRoom (element) {
     element.classList.add("recommended-rooms__item--checked");
   }
 };
+
+function cancelRoom () {
+ for (var i = 0; i < recommendedRoomsLabel.length; i++) {
+   recommendedRoomsLabel[i].classList.remove("recommended-rooms__item--hide");
+
+   if (recommendedRoomsLabel[i].classList.contains("recommended-rooms__item--checked")) {
+     recommendedRoomsLabel[i].classList.remove("recommended-rooms__item--checked");
+     recommendedRoomsBlock.insertBefore(recommendedRoomsLabel[i], recommendedRoomsBlock.firstChild);
+   }
+ };
+
+ recommendedRoomsBlock.classList.add("recommended-rooms--choose-js");
+ recommendedRoomsBlock.setAttribute("data-heading", "Рекомендованные переговорки");
+}
