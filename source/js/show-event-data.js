a28dd8c3156b5f6f0de7a593e119813bb182bca4
Kildyashov <denis.kildiaschov@yandex.ru>
2018-09-03 13:03:48 +0300

    Исправляет js для eslint - убирает this

--- source/js/show-event-data.js
+++ source/js/show-event-data.js
@@ -2,17 +2,17 @@
 
 var ESC_KEYCODE = 27;
 
-var timelineEventBlocked = document.querySelectorAll('.timeline__item--blocked');
+var timelineEventBlockes = document.querySelectorAll('.timeline__item--blocked');
 
 addListenerToEveryBlockedEvent();
 
 function addListenerToEveryBlockedEvent() {
 
-  for (var i = 0; i < timelineEventBlocked.length; i++) {
+  for (var i = 0; i < timelineEventBlockes.length; i++) {
 
-    timelineEventBlocked[i].addEventListener('click', function () {
-      debugger;
-      var currentTimeline = this;
+    timelineEventBlockes[i].addEventListener('click', function (evt) {
+
+      var currentTimeline = evt.target;
       var currentEventData = currentTimeline.querySelector('.event-data');
       var removeClass = 'event-data--show';
 
